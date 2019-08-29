4f30b4f74cdee34ad099f46f3d97d5d4790ccf78
Guama1239 <guama1239@gmail.com>
2019-08-29 12:45:27 -0500

    Initial java test demo file and its different formats

---
+++ HelloWorld.java
@@ -0,0 +1,22 @@
+/****************
+* Luis Rosales
+* CS200
+* Fall 2019
+* HW01 - HelloWorld.java
+
+***************/
+public class HelloWorld //
+{//public is visibility modifiers // static non visibility modifier // void return type // main: method name // (String[] args) parameters: type: String type: args
+   public static void main(String[] args)
+   {
+      System.out.print("Hello World");
+      System.out.println("My name is Luis Rosales"); //ln line break
+      System.out.println("Welcome to CS 200.");
+      System.out.println(2+"2");
+      System.out.print(2+2);
+   }
+}
+/*******************************
+MULTI 
+LINE COMMENT 
+****************************/
