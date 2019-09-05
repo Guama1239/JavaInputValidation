29500cd272bd8c095136d943aa39ec1fb5dd89e8
Guama1239 <guama1239@gmail.com>
2019-09-04 19:14:44 -0500

    Scanner

---
+++ SumSpaced.java
@@ -0,0 +1,25 @@
+/****************
+* Luis Rosales
+* CS200
+* Fall 2019
+* Problem 1 SumSpaced.java
+
+***************/
+public class SumSpaced //
+{//public is visibility modifiers // static non visibility modifier // void return type // main: method name // (String[] args) parameters: type: String type: args
+   public static void main(String[] args)
+   {
+      System.out.print("Hello World ");
+      System.out.println("My name is Luis Rosales"); //ln line break
+      System.out.println("Welcome to CS 200.");
+      System.out.println("You entered 12 and 11");
+      System.out.print("The product is ".concat("12*11")); 
+      //System.out.println(12*11);
+      System.out.print("The sum is ".concat("12+11"));
+      //System.out.print(12+11);
+   }
+}
+/*******************************
+MULTI
+LINE COMMENT
+****************************/
